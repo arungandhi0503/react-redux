ad39fef80f8ce26b8bb98c4af9ec444eafd3e08d
arungandhi0503 <61384255+arungandhi0503@users.noreply.github.com>
2024-09-20 20:21:27 +0200

    react bootstrap UI form initial commit for redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from "./components/Navbar";
+import AddTask from "./components/AddTask";
+import TasksList from "./components/TasksList";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Navbar />
+      <div className="row justify-content-md-center">
+        <div className='col-lg-6'>
+          <AddTask />
+          <TasksList />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TasksList.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import UpdateModal from './UpdateTask';
+
+const TasksList = () => {
+    const updateTask = () => {
+        console.log("update Task");
+        setModalShow(true)
+    };
+
+    const deleteTask = () => {
+        console.log("delete task");
+    };
+
+    const [modalShow, setModalShow] = useState(false)
+    return (
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr className="text-center">
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="text-center">
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <Button
+                                variant="primary"
+                                className="mx-3"
+                                onClick={() => updateTask()}
+                            >
+                                <i class="bi bi-pencil-fill"></i>
+                            </Button>
+                            <Button variant="primary">
+                                <i className="bi bi-trash3" onClick={() => deleteTask()}></i>
+                            </Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+
+            <UpdateModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+        </>
+    );
+};
+
+export default TasksList;
